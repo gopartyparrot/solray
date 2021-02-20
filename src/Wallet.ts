5eca6331f59bcb795b2508f22a7420c75431f0f7
De Facto <defactojob@tutanota.com>
2021-02-20 18:15:38 +0800

    Wallet.fromAccount

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,29 +1,41 @@
 // @ts-ignore
-import nacl from 'tweetnacl';
+import nacl from "tweetnacl";
 
-import * as bip39 from 'bip39';
-import * as bip32 from 'bip32';
+import * as bip39 from "bip39";
+import * as bip32 from "bip32";
 
-import {
-  Account,
-  Connection,
-  PublicKey,
-} from '@solana/web3.js';
+import { Account, Connection, PublicKey } from "@solana/web3.js";
 
-import { System } from './System';
+import { System } from "./System";
+
+function pathToAccount(path: bip32.BIP32Interface): Account {
+  return new Account(
+    nacl.sign.keyPair.fromSeed(path.privateKey as any).secretKey,
+  );
+}
 
 export class Wallet {
   static generateMnemonic(bits = 128): string {
-    return bip39.generateMnemonic(bits)
+    return bip39.generateMnemonic(bits);
   }
 
-  static async fromMnemonic(mnemonic: string, conn: Connection): Promise<Wallet> {
+  static async fromMnemonic(
+    mnemonic: string,
+    conn: Connection,
+  ): Promise<Wallet> {
     if (!bip39.validateMnemonic(mnemonic)) {
-      throw new Error('Invalid seed words');
+      throw new Error("Invalid seed words");
     }
 
     const seed = await bip39.mnemonicToSeed(mnemonic);
     return Wallet.fromSeed(seed, conn);
+  }
+
+  static fromAccount(account: Account, conn: Connection): Wallet {
+    const base = bip32
+      .fromSeed(Buffer.from(account.secretKey))
+      .derivePath(`m/501'/0'/0`);
+    return new Wallet(account, base, conn);
   }
 
   static fromSeed(seed: Buffer, conn: Connection): Wallet {
@@ -32,39 +44,39 @@ export class Wallet {
     // the secret key of its parent.
     //
     // TLDR: treat hardened paths as "accounts" that are firewalled.
-    const base = bip32
-      .fromSeed(seed)
-      .derivePath(`m/501'/0'/0`)
+    const base = bip32.fromSeed(seed).derivePath(`m/501'/0'/0`);
 
-    return new Wallet(base, conn)
+    return new Wallet(pathToAccount(base), base, conn);
   }
 
-  private sys: System
+  private sys: System;
 
-  public account: Account
-  constructor(public base: bip32.BIP32Interface, public conn: Connection) {
-    this.sys = new System(this)
-    this.account = new Account(nacl.sign.keyPair.fromSeed(base.privateKey as any).secretKey)
+  constructor(
+    public account: Account,
+    public base: bip32.BIP32Interface,
+    public conn: Connection,
+  ) {
+    this.sys = new System(this);
   }
 
   public get address(): string {
-    return this.pubkey.toBase58()
+    return this.pubkey.toBase58();
   }
 
   public get pubkey(): PublicKey {
-    return this.account.publicKey
+    return this.account.publicKey;
   }
 
   public derive(subpath: string): Wallet {
-    const child = this.base.derivePath(subpath)
-    return new Wallet(child, this.conn)
+    const child = this.base.derivePath(subpath);
+    return new Wallet(pathToAccount(child), child, this.conn);
   }
 
   public deriveAccount(subpath: string): Account {
-    return this.derive(subpath).account
+    return this.derive(subpath).account;
   }
 
   public async info(subpath?: string) {
-    return this.sys.accountInfo(this.account.publicKey)
+    return this.sys.accountInfo(this.account.publicKey);
   }
 }
