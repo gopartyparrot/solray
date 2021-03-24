3ff1d9875f121f281ab53eff05d090f3abd8b0da
De Facto <defactojob@tutanota.com>
2021-03-24 10:16:21 +0800

    change API design for ProgramAccount

--- src/ProgramAccount.ts
+++ src/ProgramAccount.ts
@@ -1,21 +1,29 @@
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 
 // ProgramAccount generates a pubkey that a program may use to invoke another program.
 export class ProgramAccount {
   // NB: for simplicity, only support nonced seeds. In rust use the corresponding
   // function `Pubkey::find_program_address(&[&seed], &program_id);`
-  static async forSeed(seed: Buffer, programID: PublicKey): Promise<ProgramAccount> {
+  static async forSeeds(
+    seeds: Buffer[],
+    programID: PublicKey,
+  ): Promise<ProgramAccount> {
     // There is ~50% probability that any given seed would fail to generate a point
     // not on the ed25539 curve. `findProgramAddress` would decrement a nonce
     // to find a working seed.
-    const [pubkey, nonce] = await PublicKey.findProgramAddress([seed], programID)
+    //
+    // Note: The nounce counts down from 255
+    if (seeds.some((seed) => seed.length > 32)) {
+      // this seems like a pretty arbitrary limit imposed by the implementation
+      throw new Error("a program seed cannot be larger than 32 bytes");
+    }
 
-    // copy the original seed and append the nonce in the last position
-    const noncedSeed = Buffer.alloc(seed.length + 1)
-    seed.copy(noncedSeed)
-    noncedSeed[seed.length] = nonce
+    const [pubkey, nonce] = await PublicKey.findProgramAddress(
+      seeds,
+      programID,
+    );
 
-    return new ProgramAccount(pubkey, noncedSeed, programID)
+    return new ProgramAccount(pubkey, seeds, nonce, programID);
   }
 
   // the nonced seed must generate a program pubkey not on the ed25539 curve
@@ -26,13 +34,18 @@ export class ProgramAccount {
   // }
 
   // The nonced seed must generate a program pubkey not on the ed25539 curve
-  constructor(public pubkey: PublicKey, public noncedSeed: Buffer, public programID: PublicKey) {}
+  constructor(
+    public pubkey: PublicKey,
+    public seeds: Buffer[],
+    public nonce: number,
+    public programID: PublicKey,
+  ) {}
 
   public get address(): string {
-    return this.pubkey.toBase58()
+    return this.pubkey.toBase58();
   }
 
-  public get nonce(): number {
-    return this.noncedSeed[this.noncedSeed.length - 1]
-  }
+  // public get nonce(): number {
+  //   return this.noncedSeeds[this.noncedSeeds.length - 1][0]
+  // }
 }
