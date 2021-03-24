d4ed255f5317f6fad2cf33a5a0826f7c936039ad
De Facto <defactojob@tutanota.com>
2021-03-24 10:16:06 +0800

    add helpers to bpf loader

--- src/BPFLoader.ts
+++ src/BPFLoader.ts
@@ -6,13 +6,26 @@ import {
 
 import { Wallet } from '.';
 
+import { promises as fs } from "fs";
+import { program } from "commander";
+
 export class BPFLoader {
+  static programID = BPF_LOADER_PROGRAM_ID;
 
-  static programID = BPF_LOADER_PROGRAM_ID
+  constructor(private wallet: Wallet, public programID = BPFLoader.programID) {}
 
-  constructor(private wallet: Wallet, public programID = BPFLoader.programID) { }
+  public async loadFile(
+    binaryPath: string,
+    programAccount = new Account(),
+  ): Promise<Account> {
+    const bin = await fs.readFile(binaryPath);
+    return this.load(bin, programAccount);
+  }
 
-  public async load(programBinary: Buffer, programAccount = new Account()): Promise<Account> {
+  public async load(
+    programBinary: Buffer,
+    programAccount = new Account(),
+  ): Promise<Account> {
     await BpfLoader.load(
       this.wallet.conn,
       this.wallet.account,
